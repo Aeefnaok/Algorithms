f8f4f4dcb1efa5ed9d389bb6f5c1db5df989600d
Ivan Chupriyanov <Aerocoolbye@yandex.ru>
2021-05-21 18:31:20 +0700

    lesson_2

--- task_1.py
+++ task_1.py
@@ -1,12 +1,15 @@
-# 1. Найти сумму и произведение цифр трехзначного числа,
-#   которое вводит пользователь.
-# https://drive.google.com/file/d/12bOq2NRP2JbBZN8eEmV1qrrGcU3QNQRg/view?usp=sharing
-num = int(input("Введите трехзначное число: "))
+# 2. Посчитать четные и нечетные цифры введенного натурального числа.
+# Например, если введено число 34560, то у него 3 четные цифры (4, 6 и 0) и
+# 2 нечетные (3 и 5).
+# https://app.diagrams.net/#G1_j-X6LEciGbtFAqCehj9vbLckGHI281_
 
-num_1 = num % 10
-num_2 = num % 100 // 10
-num_3 = num // 100
-summ = num_1 + num_2 + num_3
-prod = num_1 * num_2 * num_3
-print(f"Сумма цифр введенного числа: {summ}")
-print(f"Произведение цифр введенного числа: {prod}")
+number = input('Введите число: ')
+even = 0
+odd = 0
+for f in number:
+    i = int(f)
+    if i % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+print(f'У введенного числа {number}: четных цифр - {even}, нечетных - {odd} ')
--- task_3.py
+++ task_3.py
@@ -1,26 +1,14 @@
-# 5. Пользователь вводит две буквы. Определить, на каких местах алфавита они
-# стоят и сколько между ними находится букв.
+# 8. Посчитать, сколько раз встречается определенная цифра в введенной
+# последовательности чисел. Количество вводимых чисел и цифра, которую необходимо
+# посчитать, задаются вводом с клавиатуры.
 
-letter_1, letter_2 = [
-  x.upper() for x in input('Введите две буквы, через пробел (A - Z): ').split()
-]
 
-# Генерация списка букв
-abc_list = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
+u_range = input('Введите последовательность: ')
+u_find = input('Введите цифру для поиска: ')
+count = 0
 
-index_letter_1 = abc_list.index(letter_1) + 1
-index_letter_2 = abc_list.index(letter_2) + 1
+for i in u_range:
+    if i == u_find:
+        count += 1
 
-if index_letter_1 < index_letter_2:
-    step = 1
-else:
-    step = -1
-
-print(f'Первая буква {letter_1}, находится на позиции: {index_letter_1}')
-print(f'Вторая буква {letter_2}, находится на позиции: {index_letter_2}')
-
-print(
-    f'Между ними находятся буквы \
-{abc_list[abc_list.index(letter_1) + step:abc_list.index(letter_2):step]} \
-({abs(index_letter_1 - index_letter_2) - 1})'
-        )
+print(f'Цифра встречается {u_find} в последовательности {u_range}: {count} раз.')
