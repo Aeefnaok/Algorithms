79f348a7ab867bced91540e0e88dca9415375eee
Ivan Chupriyanov <Aerocoolbye@yandex.ru>
2021-05-28 20:29:54 +0700

    lesson_4

--- task_1.py
+++ task_1.py
@@ -1,10 +1,124 @@
-# 1. В диапазоне натуральных чисел от 2 до 99 определить, сколько из них
-# кратны каждому из чисел в диапазоне от 2 до 9.
+# 1. Проанализировать скорость и сложность одного - трёх любых алгоритмов,
+#  разработанных в рамках домашнего задания первых трех уроков.
 
-result = {}
-for i in range(2, 10):
-    result[i] = []
-    for j in range(2, 100):
-        if j % i == 0:
-            result[i].append(j)
-    print(f'Для числа {i} кратны - {len(result[i])}. Кратные числа: {result[i]}.')
+# Решил не выбирать задание из уже усделанных ранее а выполнить задание на создание матриц
+
+import random
+import timeit
+import cProfile
+import sys
+
+
+# №1 создание матрицы двумя циклами
+def matrix_1(num_1, num_2):
+    matrix = [[random.randint(1, 100) for _ in range(1, num_2)] for _ in range(1, num_1)]
+
+
+print(timeit.timeit('matrix_1(10, 10)', number=1000, globals=globals()))  # 0.08452449999999999
+print(timeit.timeit('matrix_1(100, 100)', number=1000, globals=globals()))  # 10.5874502
+
+cProfile.run('matrix_1(100, 100)')
+
+
+# Ordered by: standard name
+#
+# ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#      1    0.000    0.000    0.021    0.021 <string>:1(<module>)
+#   9801    0.007    0.000    0.014    0.000 random.py:200(randrange)
+#   9801    0.004    0.000    0.017    0.000 random.py:244(randint)
+#   9801    0.005    0.000    0.007    0.000 random.py:250(_randbelow_with_getrandbits)
+#      1    0.000    0.000    0.021    0.021 task_1.py:13(m_create_2_for)
+#      1    0.000    0.000    0.021    0.021 task_1.py:14(<listcomp>)
+#      1    0.000    0.000    0.021    0.021 {built-in method builtins.exec}
+#   9801    0.001    0.000    0.001    0.000 {method 'bit_length' of 'int' objects}
+#      1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+#  12470    0.001    0.000    0.001    0.000 {method 'getrandbits' of '_random.Random' objects}
+
+# --------------------------------------------------------------------------------------
+# №2 Создание матрицы 1 циклом
+def matrix_2(size_matrix):
+    matrix = []
+    temp_array = []
+
+    for i in range(1, (size_matrix ** 2) + 1):
+        val = random.randint(1, 100)
+
+        if i % size_matrix == 0:
+            temp_array.append(val)
+            matrix.append(temp_array)
+            temp_array = []
+        else:
+            temp_array.append(val)
+
+    return matrix
+
+
+print(timeit.timeit('matrix_2(10)', number=1000, globals=globals()))  # 0.12374689999999999
+print(timeit.timeit('matrix_2(100)', number=1000, globals=globals()))  # 12.6747321
+
+cProfile.run('matrix_2(100)')
+
+#       62900 function calls in 0.024 seconds
+#
+# Ordered by: standard name
+#
+# ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#      1    0.000    0.000    0.024    0.024 <string>:1(<module>)
+#  10000    0.007    0.000    0.014    0.000 random.py:200(randrange)
+#  10000    0.004    0.000    0.017    0.000 random.py:244(randint)
+#  10000    0.005    0.000    0.007    0.000 random.py:250(_randbelow_with_getrandbits)
+#      1    0.005    0.005    0.024    0.024 task_1.py:41(m_create_1_for)
+#      1    0.000    0.000    0.024    0.024 {built-in method builtins.exec}
+#  10100    0.001    0.000    0.001    0.000 {method 'append' of 'list' objects}
+#  10000    0.001    0.000    0.001    0.000 {method 'bit_length' of 'int' objects}
+#      1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+#  12796    0.001    0.000    0.001    0.000 {method 'getrandbits' of '_random.Random' objects}
+# ---------------------------------------------------------------------------------------------
+# №3 Создание матрицы рекурсией
+sys.setrecursionlimit(2000)
+
+
+def matrix_rec(num_1, num_2, coll_count=0, matrix_=[]):
+    matrix = []
+    if coll_count + 1 == num_2:
+        for _ in range(num_1):
+            matrix.append(random.randint(1, 100))
+        matrix_.append(matrix)
+        return matrix_
+    else:
+        for _ in range(num_1):
+            matrix.append(random.randint(1, 100))
+        coll_count += 1
+        matrix_ = matrix_rec(num_1, num_2, coll_count)
+        matrix_.append(matrix)
+        return matrix_
+
+
+print(timeit.timeit('matrix_rec(10, 10, 9)', number=1000, globals=globals()))  # 0.013299100000000008
+print(timeit.timeit('matrix_rec(100, 100, 50)', number=1000, globals=globals()))  # 5.3334564
+
+cProfile.run('matrix_rec(100, 100, 50)')
+
+#       31508 function calls (31459 primitive calls) in 0.012 seconds
+#
+# Ordered by: standard name
+#
+# ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#      1    0.000    0.000    0.012    0.012 <string>:1(<module>)
+#   5000    0.004    0.000    0.007    0.000 random.py:200(randrange)
+#   5000    0.002    0.000    0.009    0.000 random.py:244(randint)
+#   5000    0.003    0.000    0.004    0.000 random.py:250(_randbelow_with_getrandbits)
+#   50/1    0.002    0.000    0.012    0.012 task_1.py:82(m_create_recursion)
+#      1    0.000    0.000    0.012    0.012 {built-in method builtins.exec}
+#   5050    0.001    0.000    0.001    0.000 {method 'append' of 'list' objects}
+#   5000    0.000    0.000    0.000    0.000 {method 'bit_length' of 'int' objects}
+#      1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+#   6405    0.001    0.000    0.001    0.000 {method 'getrandbits' of '_random.Random' objects}
+
+# Итог: сложность данного алгоритма так же О(N**2), при этом на небольшом размере матрицы (10, 10) он медленнее
+# алгоритмов №1 и №2, но на размере 100 и >, он выигрывает по времени у алгоритма №2, но для таких значений требуется
+# увеличение стека для рекурсии, так как при стандартном размере стек переполняется, не давая алгоритму
+# выполниться.
+
+# По итогу проведения оценки скорости работы алгоритмов можно сказать, что наиболее оптимальным
+# алгоритмом для создания матриц является алгоритм №1
--- task_2.py
+++ task_2.py
@@ -1,16 +1,105 @@
-# Во втором массиве сохранить индексы четных элементов первого массива.
-# Например, если дан массив со значениями 8, 3, 15, 6, 4, 2,
-# второй массив надо заполнить значениями 0, 3, 4, 5, (индексация начинается с нуля),
-# т.к. именно в этих позициях первого массива стоят четные числа.
+# Написать два алгоритма нахождения i-го по счёту простого числа.
+# Функция нахождения простого числа должна принимать на вход натуральное и возвращать соответствующее простое число.
+# Проанализировать скорость и сложность алгоритмов.
+# Первый — с помощью алгоритма «Решето Эратосфена».
 
-import random
+import math
+import timeit
+import cProfile
 
-rand = [random.randint(-100, 100) for i in range(10)]
-print(f'Первый массив {rand}')
-index_even = []
 
-for i in rand:
-    if i % 2 == 0:
-        index_even.append(rand.index(i))
+def sieve_without_eratosthenes(i):
+    '''Функция поиска i-го простого числа,
+    без использования алгоритма «Решето Эратосфена»
+    '''
 
-print(f'Индексы чётных элементов первого массива: {index_even}')
+    lst_prime = [2]
+    number = 3
+    while len(lst_prime) < i:
+        flag = True
+        for j in lst_prime.copy():
+            if number % j == 0:
+                flag = False
+                break
+        if flag:
+            lst_prime.append(number)
+        number += 1
+    return lst_prime[-1]
+
+
+def sieve_eratosthenes(i):
+    '''Функция поиска i-го простого числа,
+    используя алгоритм «Решето Эратосфена»
+    '''
+
+    i_max = prime_counting_function(i)
+    lst_prime = [_ for _ in range(2, i_max)]
+
+    for number in lst_prime:
+        if lst_prime.index(number) <= number - 1:
+            for j in range(2, len(lst_prime)):
+                if number * j in lst_prime[number:]:
+                    lst_prime.remove(number * j)
+        else:
+            break
+    return lst_prime[i - 1]
+
+
+def prime_counting_function(i):
+    number_of_primes = 0
+    number = 2
+    while number_of_primes <= i:
+        number_of_primes = number / math.log(number)
+        number += 1
+    return number
+
+
+user_number = int(input('Введите номер по счету простого числа: '))
+print(sieve_without_eratosthenes(user_number))
+
+print('Алгоритм 1 без использования алгоритма «Решето Эратосфена»')
+print(
+    f'{sieve_without_eratosthenes(user_number)} - {user_number} \
+по счёту простое число'
+)
+
+print('Алгоритм 2 с использованием алгоритма «Решето Эратосфена»')
+print(
+    f'{sieve_eratosthenes(user_number)} - {user_number} по счёту простое число')
+
+# print(timeit.timeit('sieve_without_eratosthenes(100)', number=1000, globals=globals()))  # 0.7230837999999999
+
+# cProfile.run('sieve_without_eratosthenes(100)')
+#       1182 function calls in 0.001 seconds
+#
+# Ordered by: standard name
+#
+# ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#      1    0.000    0.000    0.001    0.001 <string>:1(<module>)
+#      1    0.001    0.001    0.001    0.001 task_2.py:11(sieve_without_eratosthenes)
+#      1    0.000    0.000    0.001    0.001 {built-in method builtins.exec}
+#    540    0.000    0.000    0.000    0.000 {built-in method builtins.len}
+#     99    0.000    0.000    0.000    0.000 {method 'append' of 'list' objects}
+#    539    0.000    0.000    0.000    0.000 {method 'copy' of 'list' objects}
+#      1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+
+# print(timeit.timeit('sieve_eratosthenes(100)', number=1000, globals=globals()))  #12.3186172
+
+# cProfile.run('sieve_eratosthenes(100)')
+
+#       1418 function calls in 0.012 seconds
+#
+# Ordered by: standard name
+#
+# ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#      1    0.000    0.000    0.012    0.012 <string>:1(<module>)
+#      1    0.010    0.010    0.011    0.011 task_2.py:30(sieve_eratosthenes)
+#      1    0.000    0.000    0.000    0.000 task_2.py:36(<listcomp>)
+#      1    0.000    0.000    0.000    0.000 task_2.py:48(prime_counting_function)
+#      1    0.000    0.000    0.012    0.012 {built-in method builtins.exec}
+#    118    0.000    0.000    0.000    0.000 {built-in method builtins.len}
+#    647    0.000    0.000    0.000    0.000 {built-in method math.log}
+#      1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+#    118    0.000    0.000    0.000    0.000 {method 'index' of 'list' objects}
+#    529    0.001    0.000    0.001    0.000 {method 'remove' of 'list' objects}
