9135f17007320e69cb32e41b1cd380b13d808312
Ivan Chupriyanov <Aerocoolbye@yandex.ru>
2021-06-11 19:50:27 +0700

    lesson_8

--- task_1.py
+++ task_1.py
@@ -1,46 +1,16 @@
-# 1. Отсортируйте по убыванию методом "пузырька" одномерный целочисленный
-# массив, заданный случайными числами на промежутке [-100; 100). Выведите на
-# экран исходный и отсортированный массивы. Сортировка должна быть реализована в
-# виде функции. По возможности доработайте алгоритм (сделайте его умнее).
+# 1) Определение количества различных подстрок с использованием хеш-функции.
+# Пусть на вход функции дана строка. Требуется вернуть количество различных подстрок в этой строке.
+# Примечание: в сумму не включаем пустую строку и строку целиком.
 
-from random import randint
-from timeit import timeit
+import hashlib
 
-MAX_SIZE = 10
-NUMBER_EXECUTIONS = 10_000
+string = input('Введите строку: ')
 
+sum_substring = set()
 
-def bubble_sort(data):
-    for i in range(len(data) - 1, 0, -1):
-        flag = True
-        for n in range(i):
-            if data[n] > data[n+1]:
-                data[n], data[n+1] = data[n+1], data[n]
-                flag = False
+for i in range(len(string)):
+    for j in range(len(string), i, -1):
+        hash_str = hashlib.sha1(string[i:j].encode('utf-8')).hexdigest()
+        sum_substring.add(hash_str)
 
-        if flag == True:
-            break
-    return data
-
-
-def bubble_sort_slow(data):
-    for i in range(len(data) - 1, 0, -1):
-        for n in range(i):
-            if data[n] > data[n+1]:
-                data[n], data[n+1] = data[n+1], data[n]
-
-    return data
-
-
-numbers = [randint(-100, 100) for _ in range(MAX_SIZE)]
-print(numbers)
-print(bubble_sort(numbers))
-
-time1 = timeit(f'bubble_sort({numbers})',
-              setup='from __main__ import bubble_sort',
-              number=NUMBER_EXECUTIONS)
-time2 = timeit(f'bubble_sort_slow({numbers})',
-              setup='from __main__ import bubble_sort_slow',
-              number=NUMBER_EXECUTIONS)
-print(time1)
-print(time2)
+print(f'{len(sum_substring) -1} подстрок в строке {string}')
